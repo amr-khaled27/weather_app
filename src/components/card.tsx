af10e39d2e559043277c434df0caaa1f02ecd448
Amr Khaled <118026839+amr-khaled27@users.noreply.github.com>
2024-12-17 11:02:37 -0800

    Update card.tsx

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,7 +6,7 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import Error from "./error";
 import { weatherData } from "./page";
 
-const api_key = "a67490cc9a235310074109d4096eb9ce";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 let coolDown = false;
 
